fb151d7ce4fae12be00ead952b63f0d24afdb4a0
Pawan <154042595+This-is-pawan@users.noreply.github.com>
2025-05-28 18:30:02 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,29 +8,29 @@ const { userRoter } = require('./routes/userRoute');
 
 const app = express();
 
-// Middleware (order matters!)
-// app.use(cors({
-//    origin: 'https://authentication-user-mern.onrender.com',
-//   credentials: true,
-// }));
-
-
-const allowedOrigins = [
-  'http://localhost:5173', // for local dev
-  'https://authentication-user-mern.onrender.com' // for production
-];
-
+Middleware (order matters!)
 app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl)
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true, // if you're using cookies or tokens
+   origin: 'https://authentication-user-mern.onrender.com',
+  credentials: true,
 }));
+
+
+// const allowedOrigins = [
+//   'http://localhost:5173', // for local dev
+//   'https://authentication-user-mern.onrender.com' // for production
+// ];
+
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     // Allow requests with no origin (like mobile apps or curl)
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true, // if you're using cookies or tokens
+// }));
 
 app.use(cookieParser());
 
