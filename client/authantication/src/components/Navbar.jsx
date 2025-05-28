e3dd672a3075124b7622e2d3891feeb8d845284b
Pawan <154042595+This-is-pawan@users.noreply.github.com>
2025-05-28 18:47:30 +0530

    Update Navbar.jsx

--- client/authantication/src/components/Navbar.jsx
+++ client/authantication/src/components/Navbar.jsx
@@ -65,7 +65,7 @@ const Navbar = () => {
 
   return (
     <div className="w-full flex justify-between p-4 sm:p-6 sm:px-24 absolute top-0">
-      <img src={assets.logo} alt="logo" className="w-28 sm:w-32" />
+      <img src={`${backendUrl}/assets.logo`} alt="logo" className="w-28 sm:w-32" />
       {userData ? (
         <div className="relative group">
           <div className="w-8 h-8 flex justify-center items-center rounded-full bg-black text-white cursor-pointer">
@@ -93,7 +93,7 @@ const Navbar = () => {
           onClick={() => navigate("/login")}
           className="flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all"
         >
-          Login <img src={assets.arrow_icon} alt="arrow-icon" />
+          Login <img src={`${backendUrl}/assets.arrow_icon`} alt="arrow-icon" />
         </button>
       )}
     </div>
